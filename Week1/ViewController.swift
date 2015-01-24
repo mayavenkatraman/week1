91d1ff8181be7d5af1e4f825cf2a31f2497b7166
mayav <mayav@yahoo-inc.com git config --global user.name mayavenkaraman git config --global user.email mayav@yahoo-inc.com>
2015-01-23 17:06:00 -0800

    Week 1 project
    week 1 project

---
+++ Week1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Week1
+//
+//  Created by Maya Venkatraman on 1/23/15.
+//  Copyright (c) 2015 Maya Venkatraman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
